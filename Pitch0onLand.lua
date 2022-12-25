e92796f332c8c035a9f66f5eb0bb93f81b9a1d10
LoseYouNow <46924287+anyeyongheng@users.noreply.github.com>
2022-12-25 15:33:37 +0800

    Update Pitch0onLand.lua

--- Pitch0onLand.lua
+++ Pitch0onLand.lua
@@ -1,6 +1,36 @@
 --code by @Zixtty1887 - https://aimware.net/forum/user/346116
 --AIMWARE  
+local function isVisible(entity)
+    if not entity then
+        return false
+    end
 
+    if entity:IsDormant() or not entity:IsAlive() then
+        return false
+    end
+
+    local lp = entities.GetLocalPlayer()
+    if not lp then
+        return false
+    end
+
+    local start = lp:GetBonePosition(8)
+    local _end = entity:GetBonePosition(8)
+
+    local trace = engine.TraceLine(start, _end, 0xFFFFFFFF)
+    local index = trace.entity and trace.entity:GetIndex() or -1
+
+    return index == entity:GetIndex()
+end
+local function esp(EspBuilder) 
+    local ent = EspBuilder:GetEntity();
+
+    --EspBuilder:Color(255,0,0,255);
+    --EspBuilder:AddTextRight(tostring(isVisible(ent)));
+   
+ end
+
+ 
 local misc_tab = gui.Tab(gui.Reference("Misc"), "ai_ragebot", "AI_Rage");
 local maingroup = gui.Groupbox(misc_tab, "Main", 16, 10, 297, 200)
 local pitch_0_onland = gui.Checkbox(maingroup, "pitch_0_onland", "Pitch 0 On Land", false)
@@ -8,7 +38,7 @@ local pitch_0_onland_second = gui.Slider(maingroup, "pitch_0_onland_second", "Ma
 
 local cache_pitch = 0;
 local pitchTime = common.Time()
-local function move()
+local function move(UserCmd)
     if pitch_0_onland:GetValue() then
         pitch_0_onland_second:SetInvisible(false);
     else
@@ -24,31 +54,35 @@ local function move()
 
     local localplayer = entities.GetLocalPlayer();
     local pitch = localplayer:GetPropVector("localdata","m_vecVelocity[0]").z;
-    if cache_pitch ~= 0 and pitch == 0 and pitch_0_onland:GetValue() and localplayer:GetWeaponType() ~=0 then
-    
-        local players = entities.FindByClass("CCSPlayer");
-        local localPosition = entities.GetLocalPlayer():GetAbsOrigin()
-        local angViewAngles = engine.GetViewAngles()
-        for i = 1, #players do
-            local player = players[i];
-            if player:IsDormant() == false and player:GetTeamNumber() ~= entities.GetLocalPlayer():GetTeamNumber() and player:IsAlive() then
-                local playerPosition = player:GetAbsOrigin()
-                local endPos = engine.TraceLine(localPosition, playerPosition).endpos
-                local content =  engine.GetPointContents(endPos, 0xFFFFFFFF)
-                if content == 0 then
-                    pitchTime = common.Time()
+    if cache_pitch ~= 0 and pitch == 0 and pitch_0_onland:GetValue() and localplayer:GetWeaponType() ~= 0  then
+        if bit.band(UserCmd.buttons, bit.lshift(1, 0)) ~= 1 then -- not shooting
+            local players = entities.FindByClass("CCSPlayer");
+            local localPosition = entities.GetLocalPlayer():GetAbsOrigin()
+            for i = 1, #players do
+                local player = players[i];
+                if player:IsDormant() == false and player:GetTeamNumber() ~= entities.GetLocalPlayer():GetTeamNumber() and player:IsAlive() then
+                    local playerPosition = player:GetAbsOrigin()
+                    local endPos = engine.TraceLine(localPosition, playerPosition).endpos
+                    local content =  engine.GetPointContents(endPos, MASK_SOLID)
+                    --print(content)
+                    if content == 0 then
+                        pitchTime = common.Time()
+                        break;
+                    end
+                    
                 end
-                
             end
+            
         end
     else
         gui.SetValue("rbot.antiaim.advanced.pitch",pitch_mode[2]);
     end 
-    if common.Time() - pitchTime < pitch_0_onland_second:GetValue() then
+    if common.Time() - pitchTime < pitch_0_onland_second:GetValue() and pitch == 0 then
         gui.SetValue("rbot.antiaim.advanced.pitch",pitch_mode[3]);
     end
 
     cache_pitch = pitch;
-
+    
  end
- callbacks.Register("PostMove", "Move", move);
+ callbacks.Register("CreateMove", "Move", move);
+ callbacks.Register("DrawESP", "ESP", esp);
